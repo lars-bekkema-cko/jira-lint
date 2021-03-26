e3e5c3d280398da093a8a8cf75b7629fbbd8afab
Lars Bekkema <lars.bekkema@checkout.com>
2021-03-26 11:45:54 +0000

    add debug to test regex

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-const jiraRegex = /[A-Za-z]{1,10}-\d+/;
+const regex = /[A-Za-z]{1,10}-\d+/;
 const errorMessage = `Please make sure that the PR title follows the standard: OT-XXXX <title>`;
 
 const skipBranch = (branch: string, branchesToSkip: string[]): boolean => {
@@ -18,9 +18,7 @@ async function run(): Promise<void> {
   try {
     const token = core.getInput('github-token', {required: true});
     const octokit = github.getOctokit(token);
-
     const branchesToSkip = core.getInput('skip-branches').split(',');
-
     const pullRequest = github.context.payload.pull_request;
 
     if (pullRequest == null) {
@@ -28,7 +26,6 @@ async function run(): Promise<void> {
       return;
     }
 
-    const prNumber = pullRequest.number;
     const branch = pullRequest.head.ref.replace('refs/heads/', '');
 
     core.debug(`branch -> ${branch}`);
@@ -38,16 +35,21 @@ async function run(): Promise<void> {
       core.debug(`skipped branch: ${branch} (${branchesToSkip})`);
     } else {
       const title = pullRequest.title;
+      const prNumber = pullRequest.number;
 
-      core.debug(`title -> ${title} -> ${jiraRegex.test(title)}`);
+      const regexValue = regex.test(title);
 
-      if (!jiraRegex.test(title)) {
+      core.debug(`title -> ${title} -> ${regexValue}`);
+
+      if (!regexValue) {
         core.setFailed(errorMessage);
+        core.debug(`Regex ${regex} failed with title ${title} (${regexValue})`);
         await octokit.issues.createComment({
           ...github.context.repo,
           issue_number: prNumber,
           body: errorMessage
         });
+        return;
       }
     }
   } catch (error) {
